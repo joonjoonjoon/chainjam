95b0637cf61c256cee27403981310bfca2211257
Jonatan Van Hove <joon@joon.be>
2014-02-03 13:58:41 +0100

    - removed p2, p3, p4 - changed menu logic - added re-scale on load

---
+++ js/gamelist.js
@@ -0,0 +1,12 @@
+/*
+ * Contains the list of available games
+ */
+ var _g = new Array();
+ var _games;
+ var _gamesCounter = -1;
+
+
+// DEMO GAMES
+_gamesCounter++;_g[_gamesCounter] = new Object();_g[_gamesCounter].name = 'Unity Demo Level 1';_g[_gamesCounter].path = 'UnityDemoLevel1.unity3d';_g[_gamesCounter].type = 'unity';_g[_gamesCounter].credits = '@joonturbo, @pyjamads';_g[_gamesCounter].instructions = 'Jump on each others heads.';
+_gamesCounter++;_g[_gamesCounter] = new Object();_g[_gamesCounter].name = 'Unity Demo Level 2';_g[_gamesCounter].path = 'UnityDemoLevel2.unity3d';_g[_gamesCounter].type = 'unity';_g[_gamesCounter].credits = '@joonturbo, @pyjamads';_g[_gamesCounter].instructions = 'Jump on each others heads.';
+_gamesCounter++;_g[_gamesCounter] = new Object();_g[_gamesCounter].name = 'Unity Demo Level 3';_g[_gamesCounter].path = 'UnityDemoLevel3.unity3d';_g[_gamesCounter].type = 'unity';_g[_gamesCounter].credits = '@joonturbo, @pyjamads';_g[_gamesCounter].instructions = 'Jump on each others heads.';
